fbd47548326d82737725fb703a70c81653a0e0d2
Kimberly <mccartyt1@montclair.edu>
2017-04-28 13:06:03 -0400

    Added comments

--- gray.c
+++ gray.c
@@ -1,6 +1,34 @@
+/*
+  Parallel Computing Final
+  Initial draft by Kimberly McCarty
+  Teammates:
+  Frank Chiavetta <chiavettaf1@mail.montclair.edu>
+  Ian Stypulkoski <stypulkoski1@mail.montclair.edu>,
+  Donald Carter <carterd5@montclair.edu>
+
+
+  Program loads in.ppm, and outputs a grayscale conversion as out.pgm
+
+
+  to create input file:
+  (using imagemagick)
+  convert SOURCEFILE.JPG -compress none IN.PPM
+
+
+  Compilation:
+  gcc gray.c
+  Running (on unix-y systems):
+  ./a.out
+
+  I suggest you copy and paste this code into a new file and edit it for parallel implementations
+*/
+
+//Standard includes
 #include <stdio.h>
 #include <stdlib.h>
 
+
+//Method declarations
 int *tograyscale(int* image, int width, int height);
 int *loadimage(FILE *f);
 void save_image(FILE *o, int* image, int width, int height);
@@ -8,24 +36,35 @@ int get_width(FILE *f);
 int get_height(FILE *f);
 
 int main(){
+  //Create file pointer to input
   FILE *fp, *ofp;
   fp = fopen("in.ppm", "r");
+
   /* read image */
   int *image;
   image= loadimage(fp);
   int width = get_width(fp);
   int height = get_height(fp);
   fclose(fp);
+
+
   /* convert image */
   int* grayscale;
   grayscale = tograyscale(image, width, height);
-  /* save image */
+  free(image);
+
+
+  /* save image to output file pointer */
   ofp = fopen("out.pgm", "w");
   save_image(ofp, grayscale, width, height);
+  free(grayscale);
   fclose(ofp);
+
+  //Done!
   return 0;
 }
 
+//function that loads the image as a one-dimensional list of subpixel values from the ppm file
 int *loadimage(FILE *f){
   //simple buffer
   char buff[255];
@@ -35,22 +74,24 @@ int *loadimage(FILE *f){
   int height;
   int bit_depth;
 
-  //integers for last pixel's colors
   fscanf(f, "%s", buff);
-  printf(buff);
+  //below was for debug purposes
+  //printf(buff);
   fscanf(f, "%d", &width);
   fscanf(f, "%d", &height);
-  //bit_depth not implemented, should change program to accomodate different bit_depths
+  //FIXME: bit_depth not implemented, maybe should generalize program to process different bit_depths, time permitting
   fscanf(f, "%d", &bit_depth);
   int fields = width*height*3; //three channels of color (red green blue)
   //allocate a memory buffer for that many bytes...
   int * result =  malloc(fields * sizeof(int));
   for (int i = 0; i < fields; i++){
+    //Read one string of decimals, store it as an integer, and move on
     fscanf(f, "%d", &result[i]);
   }
   return result;
 }
 
+//A simple function that checks what width is in the input file
 int get_width(FILE *f){
   //simple buffer
   rewind(f);
@@ -70,6 +111,7 @@ int get_width(FILE *f){
   return width;
 }
 
+//A simple function that checks what height is in the input file
 int get_height(FILE *f){
   //simple buffer
   rewind(f);
@@ -89,6 +131,9 @@ int get_height(FILE *f){
   return height;
 }
 
+//Take a one-dimensional array of subpixel values and the images resolution
+//And output a grayscale array
+//NOTE: Classmates, please edit this function!
 int *tograyscale(int* image, int width, int height){
   //integers for last pixel's colors
   int colors[3];
@@ -119,6 +164,8 @@ int *tograyscale(int* image, int width, int height){
   return grayscales;
 }
 
+//Take tograyscale's output, and make it into a valid pgm file
+//Writing to the file at FILE *o.
 void save_image(FILE *o, int* image, int width, int height){
   fprintf(o, "P2\n%d %d\n255\n", width, height);
   int length = width * height;
